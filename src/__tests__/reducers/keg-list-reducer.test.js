5ac93cf150642cfbbb0768787d031bd4416145cd
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-07-09 15:39:50 -0700

    second passing test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,45 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
-  test('Should return a default state if there is no action type passed into the reducer', () => {
+
+  let action;
+  const kegData = {
+    name: 'Apple Cider',
+    brand: 'Envy Apple Company',
+    price: '7.77',
+    alcoholContent: '9.5',
+    description: 'Very appley',
+    numOfPints: 10,
+    id: 1
+  }
+
+  test('Should return a default state if there is no action type passed into the reducer.', () => {
     expect(kegListReducer({}, { type:null })).toEqual({});
   });
+
+  test('Should sucessfully add new keg data to masterKegList.', () => {
+    const { name, brand, price, alcoholContent, description, numOfPints, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      description: description,
+      numOfPints: numOfPints,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        description: description,
+        numOfPints: numOfPints,
+        id: id
+      }
+    });
+  });
+
 });
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, alcoholContent, description, numOfPints, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id] : {
+          name: name,
+          brand: brand,
+          price: price,
+          alcoholContent: alcoholContent,
+          description: description,
+          numOfPints: numOfPints,
+          id: id    
+        }
+      });
+      default:
+        return state;
+  }
 };
